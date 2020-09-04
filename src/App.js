7f0d6439b9ca245e85866148e0e7b6b33f6361bf
RoopaSingam <62869375+RoopaSingam@users.noreply.github.com>
2020-09-04 10:45:52 +0530

    b

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import "./styles.css";
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Pages/Home";
 import Services from "./components/Pages/Services";
 import Products from "./components/Pages/Producsts";
 import SignUp from "./components/Pages/Signup";
 import Footer from "./components/Footer";
-import Vedio from "./components/Vedio";
 
 export default function App() {
   return (
